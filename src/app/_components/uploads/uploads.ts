b5846e484a491fccca7fc5facaf953ea7a4f107e
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-03-26 17:29:58 +0530

    fixes

--- src/app/_components/uploads/uploads.ts
+++ src/app/_components/uploads/uploads.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FileService } from '../../service/file';
+import { FormsModule } from '@angular/forms';
+import { HttpEventType } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-uploads',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './uploads.html',
   styleUrl: './uploads.css',
 })
 export class Uploads {
 
-  selectedFiles: File[] =  [];
+  selectedFiles: File[] = [];
+  uploadedFiles: any[] = [];
+  tags: string = '';
+  uploadProgress: number = 0;
+  isDragging = false;
+
 
   constructor(private fileService: FileService) { }
 
@@ -19,15 +28,60 @@ export class Uploads {
     this.selectedFiles = Array.from(event.target.files);
   }
 
-  upload() {
-    
-    this.selectedFiles.forEach(file => {
-
-      this.fileService.uploadFile(file)
-        .subscribe(res => {
-          console.log("Upload success", res);
-        });
-
+  delete(id: number) {
+    this.fileService.deleteFile(id).subscribe(() => {
+      this.uploadedFiles = this.uploadedFiles.filter(f => f.id !== id);
     });
   }
+
+  upload() {
+    this.selectedFiles.forEach(file => {
+      this.fileService.uploadFile(file, this.tags).subscribe(event => {
+
+        if (event.type === HttpEventType.UploadProgress && event.total) {
+          this.uploadProgress = Math.round((event.loaded / event.total) * 100);
+        }
+
+        if (event.type === HttpEventType.Response) {
+          this.uploadedFiles.push(event.body);
+          this.uploadProgress = 0;
+        }
+      });
+    });
+  }
+
+  loadFiles() {
+    this.fileService.getFiles().subscribe({
+      next: (res) => {
+        this.uploadedFiles = res;
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+onDragOver(event: DragEvent) {
+  event.preventDefault();
+  this.isDragging = true;
+}
+
+onDragLeave(event: DragEvent) {
+  event.preventDefault();
+  this.isDragging = false;
+}
+
+onDrop(event: DragEvent) {
+  event.preventDefault();
+  this.isDragging = false;
+
+  if (event.dataTransfer?.files) {
+    this.selectedFiles = Array.from(event.dataTransfer.files);
+  }
+}
+
+  ngOnInit() {
+    this.loadFiles();
+  }
+
+  
+  
 }
--- src/app/service/file.ts
+++ src/app/service/file.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FileService  {
-  private api = "http://localhost:8080/files";
 
-  constructor(private http: HttpClient) {
-  }
+  private api = "http://localhost:8080/api/files";
 
-  uploadFile(file: File) {
+  constructor(private http: HttpClient) { }
+
+  uploadFile(file: File, tags: string): Observable<any> {
     const formData = new FormData();
     formData.append("file", file);
+    formData.append("tags", tags);
 
-    return this.http.post(`${this.api}/upload`, formData);
+    return this.http.post(`${this.api}/upload`, formData, {
+      reportProgress: true,
+      observe: 'events'
+  });
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.api}`);
+  }
+
+  deleteFile(id: number) {
+    return this.http.delete(`${this.api}/${id}`);
   }
 }
